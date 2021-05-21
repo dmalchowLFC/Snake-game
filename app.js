1db4dc8044f3a666fa62aedb7cb3ae8f302463dc
dmalchowLFC <80545257+dmalchowLFC@users.noreply.github.com>
2021-05-21 10:39:43 -0700

    Most things are working
    Need to get the frameRate variable to take effect in order to increase the snake's speed with a level increase. Still want to update snake and apple with actual images and update the rest of the display to make the user experience a bit better

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-const frameRate = 75;
+const canvas = document.getElementById("gameplayScreen");
+const canvasContext = canvas.getContext("2d");
+const scoreboard = document.getElementById("scoreboard");
+const levelDisplay = document.getElementById("level");
 
-let canvas;
-let canvasContext;
+
+document.addEventListener("keydown", handleSnakeMovement)
+
+let frameRate = 75;
 let score = 0;
+let level = 1;
+let pause = 0;
 
 let appleX;
 let appleY;
 
-let snake = {
-    body: [
-        { x: 500, y: 700 },
-        { x: 500, y: 710 },
-        { x: 500, y: 720 },
-    ],
-    speed: 10,
-    direction: "Up",
-    width: 10,
-    height: 10,
-}
+
 
 
 
 
 window.onload = () => {
-    canvas = document.getElementById("gameplayScreen");
-    canvasContext = canvas.getContext("2d");
     findAppleCoordinates();
     setInterval(drawEverything, frameRate);
 }
@@ -34,47 +29,74 @@ window.onload = () => {
 function drawEverything() {
     canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    scoreboard.textContent = `Score: ${score}`;
+    levelDisplay.textContent = `Level: ${level}`;
     drawApple();
     drawSnake();
+    for (let i = 1; i < snake.body.length; i++) {
+        if (snake.body[0].x === snake.body[i].x && snake.body[0].y === snake.body[i].y) {
+            gameOver();
+        }
+    };
     if (getDistance(appleX, appleY, snake.body[0].x, snake.body[0].y) < snake.width) {
         handleCollision();
     }
-    if (snake.body[0].x <= 0 || snake.body[0].x >= 990) {
+    if (snake.body[0].x <= 0 || snake.body[0].x >= canvas.width) {
         gameOver();
     }
-    if (snake.body[0].y <= 0 || snake.body[0].y >= 740) {
+    if (snake.body[0].y <= 0 || snake.body[0].y >= canvas.height) {
         gameOver();
     }
 
 }
+
+// Apple Stuff
 function findAppleCoordinates() {
-    appleX = Math.floor(Math.random() * 990);
-    appleY = Math.floor(Math.random() * 740);
-    return appleX, appleY
+    findAppleX();
+    findAppleY();
+}
+function findAppleX(min, max) {
+    min = Math.ceil(05);
+    max = Math.floor(canvas.width);
+    appleX = Math.floor(Math.random() * (max - min) + min);
+    return appleX;
+}
+function findAppleY(min, max) {
+    min = Math.ceil(05);
+    max = Math.floor(canvas.height);
+    appleY = Math.floor(Math.random() * (max - min) + min);
+    return appleY;
 }
 
-function getDistance(x1, y1, x2, y2) {
-    let xDistance = x2 - x1;
-    let yDistance = y2 - y1;
-
-    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-}
 
 function drawApple() {
     canvasContext.fillStyle = "red";
     canvasContext.fillRect(appleX, appleY, 10, 10)
 }
 
+// Snake Stuff
+
+let snake = {
+    body: [
+        { x: (canvas.width / 2), y: canvas.height - 30 },
+        { x: (canvas.width / 2), y: canvas.height - 20 },
+        { x: (canvas.width / 2), y: canvas.height - 10 },
+    ],
+    speed: 10,
+    direction: "Up",
+    width: 10,
+    height: 10,
+}
+
+
 function drawSnake() {
     const snakeCopy = snake.body.map(snakePart => Object.assign({}, snakePart));
 
-
-    canvasContext.fillStyle = "green";
-    canvasContext.fillRect(snake.body[0].x, snake.body[0].y, snake.width, snake.height)
-    canvasContext.fillStyle = "green";
-    canvasContext.fillRect(snake.body[1].x, snake.body[1].y, snake.width, snake.height)
-    canvasContext.fillStyle = "green";
-    canvasContext.fillRect(snake.body[2].x, snake.body[2].y, snake.width, snake.height)
+    for (let i = 1; i < snake.body.length; i++) {
+        snake.body[i] = snakeCopy[i - 1];
+        canvasContext.fillStyle = "green";
+        canvasContext.fillRect(snake.body[i].x, snake.body[i].y, snake.width, snake.height)
+    };
 
     if (snake.direction === "Up") {
         snake.body[0].y -= snake.speed;
@@ -89,13 +111,8 @@ function drawSnake() {
         snake.body[0].x -= snake.speed;
     }
 
-    for (let i = 1; i < snake.body.length; i++) {
-        snake.body[i] = snakeCopy[i - 1];
-    };
-
 }
 
-document.addEventListener("keydown", handleSnakeMovement)
 
 function handleSnakeMovement(e) {
     if (snake.direction === "Up") {
@@ -130,20 +147,45 @@ function handleSnakeMovement(e) {
             snake.direction = "Down"
         };
     }
+
 }
 
+// Distance Stuff
+function getDistance(x1, y1, x2, y2) {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+
 function handleCollision() {
-    score += 100;
+    updateScore();
+    console.log(score)
     snake.body.push({ x: appleX, y: appleY });
     findAppleCoordinates();
-    console.log("collision detected");
-    console.log(score);
-    console.log(snake.body)
 }
 
 
 function gameOver() {
-    alert("Game Over!");
+    console.log("Game Over")
 }
 
 
+
+//Number stuff
+function updateScore() {
+    score += 10;
+    if (score % 20 === 0) {
+        updateLevel();
+    }
+    return score
+}
+
+
+function updateLevel() {
+    level += 1;
+    frameRate -= 25;
+    console.log(level);
+    console.log(frameRate)
+    return level
+};
